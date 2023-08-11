487849cbda8cf314867ccea8c1f76b5eab8c4c13
ManojkumarYem <manojravi4848@gmail.com>
2023-08-11 17:56:59 +0530

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
-console.log("hello")
 const jwt = require('jsonwebtoken');
 const db = require('./dbconnection');
 const cors = require('cors');
+const multer = require('multer');
 const path = require('path');
 const app = express();
 const PORT = 3000;
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 // app.use(express.static(path.resolve(__dirname, '../client/public')));
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -17,9 +18,37 @@ app.use(cookieParser());
 var username = null;
 var token = null;
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 1000000 }
+})
+
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
 // });
+
+// app.post("/upload/", upload.single('image_file'), (req, res) => {
+//     const empid = req.body.empid;
+//     const profile = req.file.filename;
+//     const sql = "update userdetails set profile = ? where EmpID =?"
+//     db.query(sql, [profile, empid], (err, result) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(empid);
+//             res.send(JSON.stringify(result));
+//         }
+//     })
+// })
 
 app.post("/login/", (req, res) => {
     res.setHeader('mysql', { "Content-Type": "text/plain" });
@@ -43,7 +72,7 @@ app.post("/login/", (req, res) => {
 
 app.post("/my_info/", (req, res) => {
     res.setHeader('mysql', { "Content-Type": "text/plain" });
-    const sql = "select UserDetails.EmpID, UserDetails.First_Name, UserDetails.Last_Name,  DATEDIFF(CURDATE(), DOJ)/365 as Experience, UserDetails.Designation, UserDetails.DOJ, UserDetails.Address, UserDetails.Zipcode, UserCredential.MobileNo, UserDetails.Is_Active, UserDetails.Profile from UserDetails inner join UserCredential on UserDetails.EmpID = UserCredential.EmpID where Is_Active = 1 and username = ?";
+    const sql = "select UserDetails.EmpID, UserDetails.First_Name, UserDetails.Last_Name, UserDetails.DOB, DATEDIFF(CURDATE(), DOJ)/365 as Experience, UserDetails.Designation, UserDetails.DOJ, UserDetails.Address, UserDetails.Zipcode, UserCredential.MobileNo, UserDetails.Is_Active, UserDetails.Profile from UserDetails inner join UserCredential on UserDetails.EmpID = UserCredential.EmpID where Is_Active = 1 and username = ?";
     res.setHeader("Content-Type", "text/plain");
     db.query(sql, [username], (err, result) => {
         if (err) {
@@ -56,7 +85,7 @@ app.post("/my_info/", (req, res) => {
 
 app.post("/dashboard/", (req, res) => {
     res.setHeader('mysql', { "Content-Type": "text/plain" });
-    const sql = "select UserDetails.EmpID, UserDetails.First_Name, UserDetails.Last_Name,  DATEDIFF(CURDATE(), DOJ)/365 as Experience, UserDetails.Designation, UserDetails.DOJ, UserDetails.Address, UserDetails.Zipcode, UserCredential.MobileNo, UserDetails.Is_Active, UserDetails.Profile from UserDetails inner join UserCredential on UserDetails.EmpID = UserCredential.EmpID where Is_Active = 1";
+    const sql = "select UserDetails.EmpID, UserDetails.First_Name, UserDetails.DOB, UserDetails.Last_Name,  DATEDIFF(CURDATE(), DOJ)/365 as Experience, UserDetails.Designation, UserDetails.DOJ, UserDetails.Address, UserDetails.Zipcode, UserCredential.MobileNo, UserDetails.Is_Active, UserDetails.Profile from UserDetails inner join UserCredential on UserDetails.EmpID = UserCredential.EmpID where Is_Active = 1";
     db.query(sql, (err, result) => {
         if (err) {
             console.log(err);
@@ -86,8 +115,55 @@ app.post("/show_attendance_details/", (req, res) => {
     res.setHeader('mysql', { "Content-Type": "text/plain" });
     const empid = req.body.empid;
     const date = req.body.date;
-    const sql = "select * from attendance_reports where EmpID = ? and date = ?";
-    db.query(sql, [empid, date], (err, result) => {
+    const dateTo = req.body.dateTo;
+    let sql;
+    let values;
+    if (dateTo) {
+        sql = "SELECT * FROM attendance_reports WHERE EmpID = ? AND date BETWEEN ? AND ?";
+        values = [empid, date, dateTo];
+    } else {
+        sql = "SELECT * FROM attendance_reports WHERE EmpID = ? AND date = ?";
+        values = [empid, date];
+    }
+
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+app.post("/attendance_status/", (req, res) => {
+    res.setHeader('mysql', { "Content-Type": "text/plain" });
+    const status = "Pending";
+    const sql = "select * from attendance_reports where Status = ?";
+    db.query(sql, [status], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+app.post("/attendance_status_update/", (req, res) => {
+    res.setHeader('mysql', { "Content-Type": "text/plain" });
+    const empid = req.body.empid;
+    const date = req.body.date;
+    const status_update = req.body.status_update;
+    const attendance_status = req.body.attendance_status;
+    let sql;
+    let values;
+    if (attendance_status == "Present" || attendance_status == "Not-approve") {
+        sql = "update attendance_reports set Status = ?, attendance_status = ? where EmpID = ? and Date = ?";
+        values = [status_update, attendance_status, empid, date]
+    } else {
+        sql = "update attendance_reports set Status = ?, attendance_status = ? where EmpID = ? and Date = ?";
+        values = [status_update, attendance_status, empid, date]
+    }
+    db.query(sql, values, (err, result) => {
         if (err) {
             console.log(err);
         } else {
@@ -101,16 +177,37 @@ app.post("/attendance/", (req, res) => {
     const empid = req.body.empid;
     const empname = req.body.empname;
     const designation = req.body.designation;
-    const date = req.body.date;
+    const dateFrom = req.body.dateFrom;
+    const dateTo = req.body.dateTo;
     const mode = req.body.mode;
-    const sql = "insert into attendance_reports(EmpID, EmpName, Designation, Date, mode) values(?, ?, ?, ?, ?)";
-    db.query(sql, [empid, empname, designation, date, mode], (err, result) => {
+    const Status = "Pending";
+    let sql;
+    let values = [];
+    insertQuery = "insert into attendance_reports(EmpID, EmpName, Designation, Date, Status, mode) values ?";
+    sql = "SELECT DATEDIFF(" + dateTo + "," + dateFrom + ") AS day_difference";
+    db.query("SELECT DATEDIFF(?, ?) AS day_difference", [dateTo, dateFrom], (err, result) => {
         if (err) {
             console.log(err);
-        } else {
-            res.send(JSON.stringify(result));
+            res.send("Error occurred.");
+            return;
         }
-    });
+        const dayDifference = result[0].day_difference;
+        if (dayDifference >= 0) {
+            for (let i = 0; i <= dayDifference; i++) {
+                const currentDate = new Date(dateFrom);
+                currentDate.setDate(currentDate.getDate() + i);
+                values.push([empid, empname, designation, currentDate.toISOString().split('T')[0], Status, mode]);
+            }
+
+            db.query(insertQuery, [values], (err, result) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.send(JSON.stringify(result));
+                }
+            });
+        }
+    })
 });
 
 app.post("/forget_psw/", (req, res) => {
@@ -184,6 +281,20 @@ app.post("/forget_psw/change_psw/", (req, res) => {
     });
 });
 
+app.post("/change_newPassword/", (req, res) => {
+    res.setHeader('mysql', { "Content-Type": "text/plain" });
+    const newpassword = req.body.newpassword;
+    const currentpassword = req.body.currentpassword;
+    const sql = "update usercredential set Password = ? where UserName =? and Password = ?";
+    db.query(sql, [newpassword, username, currentpassword], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
 app.post("/add&show_employee/", (req, res) => {
     res.setHeader('mysql', { "Content-Type": "text/plain" });
     const empid = req.body.empid;
@@ -191,6 +302,8 @@ app.post("/add&show_employee/", (req, res) => {
     const lastname = req.body.lastname;
     const experience = req.body.experience;
     const designation = req.body.designation;
+    const dob = req.body.dob;
+    const age = req.body.age;
     const doj = req.body.doj;
     const dod = req.body.dod;
     const address = req.body.address;
@@ -200,7 +313,7 @@ app.post("/add&show_employee/", (req, res) => {
     const password = req.body.password;
     const mobileno = req.body.mobileno;
     const otp = "NULL";
-    const mail = "my1stcompany2001@gmail.com";
+    const mail = req.body.mail;
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -208,12 +321,24 @@ app.post("/add&show_employee/", (req, res) => {
             pass: 'yauifmhmgschtpwt'
         }
     });
-    const sql = "insert into userdetails(EmpID, First_Name, Last_Name, Experience, Designation, DOJ, DOD, Address, Zipcode, Is_Active) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    const sql = "insert into userdetails(EmpID, First_Name, Last_Name, DOB, Age, Experience, Designation, DOJ, DOD, Address, Zipcode, Is_Active) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     const sql1 = "insert into usercredential(EmpID, UserName, Password, MobileNo, OTP, Mail) values(?, ?, ?, ?, ?, ?)";
-    db.query(sql, [empid, firstname, lastname, experience, designation, doj, dod, address, zipcode, is_active], (err, result) => {
+    db.query(sql, [empid, firstname, lastname, dob, age, experience, designation, doj, dod, address, zipcode, is_active], (err, result) => {
         if (err) {
             console.log(err);
         } else {
+            app.post("/upload/", upload.single('image_file'), (req, res) => {
+                const profile = req.file.filename;
+                const sql = "update userdetails set profile = ? where EmpID =?"
+                db.query(sql, [profile, empid], (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.send(JSON.stringify(result));
+                        res.end();
+                    }
+                })
+            })
             res.send(JSON.stringify(result));
         }
     });
@@ -261,6 +386,48 @@ app.post("/remove_employee/", (req, res) => {
     })
 });
 
+app.post("/absentees_list/", (req, res) => {
+    res.setHeader('mysql', { "Content-Type": "text/plain" });
+    // const dateFrom = req.body.dateFrom;
+    const date = req.body.date;
+    const attendance_status = req.body.attendance_status;
+    let sql;
+    let params;
+
+    if (attendance_status === "Present") {
+        // Query for present members
+        sql = "SELECT userdetails.empid, userdetails.First_name, userdetails.last_name, attendance_reports.date, attendance_reports.status, attendance_reports.mode FROM userdetails LEFT JOIN attendance_reports ON userdetails.empid = attendance_reports.empid AND attendance_reports.date=? WHERE attendance_reports.attendance_status='Present' AND userdetails.Is_Active=1";
+        params = [date];
+    } else if (attendance_status === "Absent") {
+        // Query for absent members
+        sql = "SELECT userdetails.empid, userdetails.First_name, userdetails.last_name, attendance_reports.date, attendance_reports.status, attendance_reports.mode FROM userdetails LEFT JOIN attendance_reports ON userdetails.empid = attendance_reports.empid AND attendance_reports.date=? WHERE attendance_reports.attendance_status='Absent' AND userdetails.Is_Active=1";
+        params = [date];
+    } else {
+        sql = "SELECT userdetails.empid, userdetails.First_name, userdetails.last_name, attendance_reports.date, attendance_reports.status, attendance_reports.mode FROM userdetails LEFT JOIN attendance_reports ON userdetails.empid = attendance_reports.empid AND attendance_reports.date=? WHERE userdetails.Is_Active=1";
+        params = [date];
+    }
+    // const sql = "SELECT userdetails.empid, userdetails.First_name,userdetails.last_name, attendance_reports.date, attendance_reports.status, attendance_reports.mode FROM userdetails left JOIN attendance_reports ON userdetails.empid = attendance_reports.empid and attendance_reports.date=? where userdetails.Is_Active =1";
+    db.query(sql, [params], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    })
+});
+
+app.post("/birth_date/", (req, res) => {
+    res.setHeader('mysql', { "Content-Type": "text/plain" });
+    const sql = " SELECT * FROM  userdetails WHERE DATE_FORMAT( CONCAT(YEAR(CURDATE()),'-',DATE_FORMAT(DOB, '%m-%d') ), '%Y-%m-%d') BETWEEN CURDATE() AND DATE_ADD(CURDATE(), INTERVAL 1 MONTH) order by DAYOFYEAR(DOB) < DAYOFYEAR(CURDATE()),DAYOFYEAR(DOB); ";
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    })
+});
+
 app.post("/edit_employee/", (req, res) => {
     res.setHeader('mysql', { "Content-Type": "text/plain" });
     const empid = req.body.empid;
@@ -284,6 +451,18 @@ app.post("/edit_employee/", (req, res) => {
         if (err) {
             console.log(err);
         } else {
+            app.post("/uploading/", upload.single('image_file_2'), (req, res) => {
+                const profile = req.file.filename;
+                const sql = "update userdetails set profile = ? where EmpID =?"
+                db.query(sql, [profile, empid], (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(empid);
+                        res.send(JSON.stringify(result));
+                    }
+                })
+            })
             db.query(sql1, [mobileno, empid], (err) => {
                 var mailOptions = {
                     from: 'my1stcompany2001@gmail.com',
@@ -319,5 +498,13 @@ app.post("/find/", (req, res) => {
         }
     });
 });
+
+// app.get("/getMysqlStatus", (req, res) => {
+//     database.ping((err) => {
+//         if (err) return res.status(500).send("MySQL Server is Down");
+
+//         res.send("MySQL Server is Active");
+//     })
+// });
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
